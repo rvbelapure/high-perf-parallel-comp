8b36aa275be424016bc307d077a5f0c34097b66a
Raghavendra Belapure <rvbelapure@gmail.com>
2013-09-12 15:17:19 -0400

    fixed nowait

--- lab2/parallel-mergesort--omp.cc
+++ lab2/parallel-mergesort--omp.cc
@@ -12,12 +12,14 @@
 #include <algorithm>
 
 #include <omp.h>
+#include <string.h>
 
 #define DEBUG 0
 
 void parallelSort(int N, keytype* A);
 void p_mergesort(keytype * A, int p, int r, keytype * B, int s);
-void parallel_merge(keytype * T, int p1, int r1, int p2, int r2, keytype * A, int p3);
+void pmerge(keytype * T, int p1, int r1, int p2, int r2, keytype * A, int p3);
+void smerge(keytype * T, int p1, int r1, int p2, int r2, keytype * A, int p3);
 int binary_search(keytype x, keytype * T, int p, int r);
 void printArray(int N, keytype * A);
 
@@ -48,7 +50,38 @@ int binary_search(keytype x, keytype * T, int p, int r)
 	return high;
 }
 
-void parallel_merge(keytype *T, int p1, int r1, int p2, int r2, keytype *A, int p3)
+void smerge(keytype *T, int p1, int r1, int p2, int r2, keytype *A, int p3)
+{		
+	int i,j,k;
+	i = p1;
+	j = p2;
+	k = p3;
+	while((i <= r1) && (j <= r2))
+	{
+		if(T[i] < T[j])
+			A[k++] = T[i++];
+		else if(T[i] == T[j])
+		{
+			A[k++] = T[i++];
+			A[k++] = T[j++];
+		}
+		else
+			A[k++] = T[j++];
+	}
+
+	if(i <= r1)
+	{
+		for( ; i <= r1 ; i++, k++)
+			A[k] = T[i];
+	}
+	if(j <= r2)
+	{
+		for( ; j <= r2 ; j++, k++)
+			A[k] = T[j];
+	}
+}
+
+void pmerge(keytype *T, int p1, int r1, int p2, int r2, keytype *A, int p3)
 {
 	int n1 = r1 - p1 + 1;
 	int n2 = r2 - p2 + 1;
@@ -60,7 +93,7 @@ void parallel_merge(keytype *T, int p1, int r1, int p2, int r2, keytype *A, int 
 		std::swap(n1, n2);
 	}
 
-	int G = 1000000;
+	int G = 100000;
 	if(n1 < G)
 	{
 		int i,j,k;
@@ -98,16 +131,15 @@ void parallel_merge(keytype *T, int p1, int r1, int p2, int r2, keytype *A, int 
 	int q2 = binary_search(T[q1], T, p2, r2);
 	int q3 = p3 + (q1 - p1) + (q2 - p2);
 	A[q3] = T[q1];
-	#pragma omp task default(none) shared(T, A, p1, q1, p2, q2, p3)
-	parallel_merge(T, p1, q1 - 1, p2, q2 - 1, A, p3);
-	#pragma omp task default(none) shared(T, A, q1, r1, q2, r2, q3)
-	parallel_merge(T, q1 + 1, r1, q2, r2, A, q3 + 1);
+	#pragma omp task default(none) shared(T, p1, q1, p2, q2, A, p3)
+	pmerge(T, p1, q1 - 1, p2, q2 - 1, A, p3);
+	pmerge(T, q1 + 1, r1, q2, r2, A, q3 + 1);
 	#pragma omp taskwait
 }
 
 void p_mergesort(keytype * A, int p, int r, keytype * B, int s)
 {
-	int G = 1000;
+	int G = 5000000;
 	int n = r - p + 1;
 	if(n < G)
 	{
@@ -116,17 +148,14 @@ void p_mergesort(keytype * A, int p, int r, keytype * B, int s)
 	}
 
 	keytype * T = newCopy(r+1, A);
-	printArray(r+1, T, "A");
 
 	int q = ((p + r) / 2);
 	#pragma omp task default(none) shared(A, T, p, q)
 	p_mergesort(A, p, q, T, p);
-	#pragma omp task default(none) shared(A, T, q, r)
 	p_mergesort(A, q + 1, r, T, q + 1);
 	#pragma omp taskwait
-	parallel_merge(T, p, q, q + 1, r, B, s);
+	smerge(T, p, q, q + 1, r, B, s);
 
-	printArray(n, B + s,"merged");
 	free(T);
 }
 
@@ -136,12 +165,9 @@ parallelSort (int N, keytype* A)
 {
 	keytype * B = newCopy(N, A);
 	#pragma omp parallel
-	#pragma omp single
+	#pragma omp single nowait
 	p_mergesort(A, 0, N-1, B, 0);
-	int i;
-	#pragma omp parallel for default(none) shared(A,B,N) private(i)
-	for(i = 0 ; i < N ; i++)
-		A[i] = B[i];
+	memcpy(A, B, N * sizeof(keytype));
 	free(B);
 	printArray(N, A, "final : ");
 }
