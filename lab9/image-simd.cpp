17b0a68712bc922c4682d8fbede0c1c6cf6930b7
Raghavendra Belapure <rvbelapure@gmail.com>
2013-11-13 02:02:15 -0500

    part 1 : fixed to floating point conversion vectorized

--- lab9/image-simd.cpp
+++ lab9/image-simd.cpp
@@ -1,8 +1,58 @@
 #include <hpcdefs.hpp>
 #include <image.hpp>
 
+static const __m128i shuff1 = _mm_set_epi8(-1, -1, -1, 3, -1, -1, -1, 2, -1, -1, -1, 1, -1, -1, -1, 0);
+static const __m128i shuff2 = _mm_set_epi8(-1, -1, -1, 7, -1, -1, -1, 6, -1, -1, -1, 5, -1, -1, -1, 4);
+static const __m128i shuff3 = _mm_set_epi8(-1, -1, -1, 11, -1, -1, -1, 10, -1, -1, -1, 9, -1, -1, -1, 8);
+static const __m128i shuff4 = _mm_set_epi8(-1, -1, -1, 15, -1, -1, -1, 14, -1, -1, -1, 13, -1, -1, -1, 12);
+static const __m128d factor = _mm_set1_pd( (double) ((double) 1.0 / (double) 255.0));
+
 void convert_to_floating_point_optimized(const uint8_t *CSE6230_RESTRICT fixed_point_images, double *CSE6230_RESTRICT floating_point_images, size_t image_width, size_t image_height, size_t image_count) {
-	for (size_t image_number = 0; image_number < image_count; image_number++) {
+
+	size_t total_Count = image_count * image_height * image_width;
+	for(size_t offset = 0 ; offset < total_Count ; offset += 16)
+	{
+		__m128i chunk = _mm_load_si128((const __m128i *) (fixed_point_images + offset));
+
+
+		__m128i part1_32 = _mm_shuffle_epi8(chunk, shuff1);
+		__m128i part2_32 = _mm_shuffle_epi8(chunk, shuff2);
+		__m128i part3_32 = _mm_shuffle_epi8(chunk, shuff3);
+		__m128i part4_32 = _mm_shuffle_epi8(chunk, shuff4);
+
+
+		__m128d part1_d_lo = _mm_cvtepi32_pd(part1_32);
+		__m128d part1_d_hi = _mm_cvtepi32_pd( _mm_srli_si128(part1_32,8) );
+
+		__m128d part2_d_lo = _mm_cvtepi32_pd(part2_32);
+		__m128d part2_d_hi = _mm_cvtepi32_pd( _mm_srli_si128(part2_32,8) );
+
+		__m128d part3_d_lo = _mm_cvtepi32_pd(part3_32);
+		__m128d part3_d_hi = _mm_cvtepi32_pd( _mm_srli_si128(part3_32,8) );
+
+		__m128d part4_d_lo = _mm_cvtepi32_pd(part4_32);
+		__m128d part4_d_hi = _mm_cvtepi32_pd( _mm_srli_si128(part4_32,8) );
+
+		part1_d_lo = _mm_mul_pd(part1_d_lo, factor);
+		part1_d_hi = _mm_mul_pd(part1_d_hi, factor);
+		part2_d_lo = _mm_mul_pd(part2_d_lo, factor);
+		part2_d_hi = _mm_mul_pd(part2_d_hi, factor);
+		part3_d_lo = _mm_mul_pd(part3_d_lo, factor);
+		part3_d_hi = _mm_mul_pd(part3_d_hi, factor);
+		part4_d_lo = _mm_mul_pd(part4_d_lo, factor);
+		part4_d_hi = _mm_mul_pd(part4_d_hi, factor);
+
+		_mm_store_pd((double *) (floating_point_images + offset), part1_d_lo);
+		_mm_store_pd((double *) (floating_point_images + offset + 2), part1_d_hi);
+		_mm_store_pd((double *) (floating_point_images + offset + 4), part2_d_lo);
+		_mm_store_pd((double *) (floating_point_images + offset + 6), part2_d_hi);
+		_mm_store_pd((double *) (floating_point_images + offset + 8), part3_d_lo);
+		_mm_store_pd((double *) (floating_point_images + offset + 10), part3_d_hi);
+		_mm_store_pd((double *) (floating_point_images + offset + 12), part4_d_lo);
+		_mm_store_pd((double *) (floating_point_images + offset + 14), part4_d_hi);
+	}
+
+/*	for (size_t image_number = 0; image_number < image_count; image_number++) {
 		for (size_t image_row = 0; image_row < image_height; image_row++) {
 			for (size_t image_column = 0; image_column < image_width; image_column++) {
 				floating_point_images[(image_number * image_height + image_row) * image_width + image_column] =
@@ -10,6 +60,7 @@ void convert_to_floating_point_optimized(const uint8_t *CSE6230_RESTRICT fixed_p
 			}
 		}
 	}
+*/
 }
 
 void matrix_vector_multiplication_optimized(double *CSE6230_RESTRICT output_vector, const double *CSE6230_RESTRICT matrix, const double *CSE6230_RESTRICT input_vector, size_t matrix_width, size_t matrix_height) {
